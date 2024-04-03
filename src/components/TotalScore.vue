0e9380a217adfcdd54da200113edaf9108d57f57
pyfbsdk59 <pyfbsdk59@gmail.com>
2024-04-03 09:10:44 +0800

    SFC21

--- src/components/TotalScore.vue
+++ src/components/TotalScore.vue
@@ -5,27 +5,27 @@
    <div v-if="cSign1">
        <table>
        <tr>
-       <td><h1>指標1</h1></td>
+       <td><h1>指標1x</h1></td>
        <td><h1>{{cSign1}}</h1></td>
        </tr>
        <tr>
-       <td><h1>指標2</h1></td>
+       <td><h1>指標2x</h1></td>
        <td><h1>{{cSign2}}</h1></td>
        </tr>
        <tr>
-       <td><h1>指標3</h1></td>
+       <td><h1>指標3x</h1></td>
        <td><h1>{{cSign3}}</h1></td>
        </tr>
        <tr>
-       <td><h1>指標4</h1></td>
+       <td><h1>指標4x</h1></td>
        <td><h1>{{cSign4}}</h1></td>
        </tr>
        <tr>
-       <td><h1>指標5</h1></td>
+       <td><h1>指標5x</h1></td>
        <td><h1>{{cSign5}}</h1></td>
        </tr>
        <tr>
-       <td><h1>指標6</h1></td>
+       <td><h1>指標6x</h1></td>
        <td><h1>{{cSign6}}</h1></td>
        </tr>
        </table>
@@ -42,11 +42,26 @@
   
     },
   
-    props: {
-        stock6data: {
-            type: Object
+    props: [{
+        'cSign1': {
+            type: String
+        },
+        'cSign2': {
+            type: String
+        },
+        'cSign3': {
+            type: String
+        },
+        'cSign4': {
+            type: String
+        },
+        'cSign5': {
+            type: String
+        },
+        'cSign6': {
+            type: String
         }
-    }, 
+    }], 
     data(){
       return {
         cSign1: "",
