1cbc05d35f668a6283f049c3dc9da2ee8cb899bf
pyfbsdk59 <pyfbsdk59@gmail.com>
2024-04-03 11:03:24 +0800

    SFC29

---
+++ src/components/Stock6Sign1.vue
@@ -0,0 +1,59 @@
+<template>
+    
+
+<div>
+<FONT SIZE="7">555555555555555555</FONT>  
+</div>
+ 
+</template>
+  
+<script>
+  
+  export default {
+    name: 'Stock6Sign1',
+    components: {
+  
+    },
+  
+    props: {
+  
+    }, 
+    data(){
+      return {
+
+    
+      }
+    },
+  
+    computed: {
+  
+    },
+  
+    methods: {       
+  
+        },      
+  
+  }
+  
+
+  
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  </style>
+  
