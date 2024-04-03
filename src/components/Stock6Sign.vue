952f5c72ec6b0833cd53cc75d3bdb46ef6e0eee9
pyfbsdk59 <pyfbsdk59@gmail.com>
2024-04-03 11:36:55 +0800

    SFC33

--- src/components/Stock6Sign.vue
+++ src/components/Stock6Sign.vue
@@ -73,15 +73,15 @@
  <br>
 
 
-
+<stock6-sign1 v-bind:cSign1="cSign1"></stock6-sign1>
 </template>
 
 <script>
-
+import Stock6Sign1 from './components/Stock6Sign1.vue'
 export default {
   name: 'Stock6Sign',
   components: {
-
+    Stock6Sign1
   },
 
   props: {
