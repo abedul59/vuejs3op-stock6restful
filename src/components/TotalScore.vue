6b2ec1ea8f519ada139f019a481774587c6a4333
pyfbsdk59 <pyfbsdk59@gmail.com>
2024-04-04 23:06:18 +0800

    SFC52

---
+++ src/components/TotalScore.vue
@@ -0,0 +1,60 @@
+<template>
+    
+
+    <div>
+    <FONT SIZE="7">666666</FONT>  
+    <FONT SIZE="7">{{ stock6data.cSign5 }}</FONT>  
+    </div>
+    
+    </template>
+      
+    <script>
+      
+      export default {
+        name: 'TotalScore',
+        components: {
+      
+        },
+      
+        props: {
+          stock6data: Object
+        }, 
+    
+        data(){
+          return {
+        
+          }
+        },
+      
+        computed: {
+      
+        },
+      
+        methods: {       
+      
+            },      
+      
+      }
+      
+    
+      
+      </script>
+      
+      <!-- Add "scoped" attribute to limit CSS to this component only -->
+      <style scoped>
+      h3 {
+        margin: 40px 0 0;
+      }
+      ul {
+        list-style-type: none;
+        padding: 0;
+      }
+      li {
+        display: inline-block;
+        margin: 0 10px;
+      }
+      a {
+        color: #42b983;
+      }
+      </style>
+      
