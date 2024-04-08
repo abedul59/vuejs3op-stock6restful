4b1c55b651aedf8c19301be2c4148c2c6f8286d1
pyfbsdk59 <pyfbsdk59@gmail.com>
2024-04-08 12:30:18 +0800

    SFC57

--- src/components/StockS1gn.vue
+++ src/components/StockS1gn.vue
@@ -1,12 +1,68 @@
 <template>
     
 
-<div>
-<FONT SIZE="7">5555555</FONT> 
-<br> 
-<FONT SIZE="7">{{ cSign6 }}</FONT>  
-</div>
 
+<table border="2" cellpadding="1" cellspacing="5"  style="border:1px solid black;margin-left:auto;margin-right:auto;">
+<caption><FONT SIZE="6">營收詳細資料</FONT></caption>
+
+
+<tr>
+<td align="center"><div><FONT SIZE="7">年月</FONT></div></td> <td align="center"><div><FONT SIZE="7">單位：%</FONT></div></td>
+</tr>
+
+<tr>
+<td align="center" style="background-color:#DCB5FF;"><div><FONT SIZE="7">111/1,2月平均</FONT></div></td> <td align="center"><div><FONT SIZE="7">{{luX}}</FONT></div></td>
+</tr>
+
+<tr>
+<td align="center" style="background-color:#DCB5FF;"><div><FONT SIZE="7">{{a1N}}</FONT></div></td> <td align="center"><div><FONT SIZE="7">{{na1}}</FONT></div></td>
+</tr>
+
+<tr>
+<td align="center" style="background-color:#DCB5FF;"><div><FONT SIZE="7">{{a2N}}</FONT></div></td> <td align="center"><div><FONT SIZE="7">{{na2}}</FONT></div></td>
+</tr>
+
+<tr>
+<td align="center" style="background-color:#DCB5FF;"><div><FONT SIZE="7">{{a3N}}</FONT></div></td> <td align="center"><div><FONT SIZE="7">{{na3}}</FONT></div></td>
+</tr>
+
+<tr>
+<td align="center" style="background-color:#DCB5FF;"><div><FONT SIZE="7">{{a4N}}</FONT></div></td> <td align="center"><div><FONT SIZE="7">{{na4}}</FONT></div></td>
+</tr>
+
+<tr>
+<td align="center" style="background-color:#DCB5FF;"><div><FONT SIZE="7">{{a5N}}</FONT></div></td> <td align="center"><div><FONT SIZE="7">{{na5}}</FONT></div></td>
+</tr>
+
+<tr>
+<td align="center" style="background-color:#DCB5FF;"><div><FONT SIZE="7">{{a6N}}</FONT></div></td> <td align="center"><div><FONT SIZE="7">{{na6}}</FONT></div></td>
+</tr>
+
+<tr>
+<td align="center" style="background-color:#DCB5FF;"><div><FONT SIZE="7">{{a7N}}</FONT></div></td> <td align="center"><div><FONT SIZE="7">{{na7}}</FONT></div></td>
+</tr>
+
+<tr>
+<td align="center" style="background-color:#FF0080;"><div><FONT SIZE="7">六個月平均</FONT></div></td> <td align="center"><div><FONT SIZE="7">{{na9}}</FONT></div></td>
+</tr>
+
+
+
+
+<tr>
+<td align="center" style="background-color:#FF0080;"><div><FONT SIZE="7">最新一個月月增率（一般情況）</FONT></div></td> <td align="center"><div><FONT SIZE="7">{{na10}}</FONT></div></td>
+</tr>
+
+
+
+
+
+<tr>
+<td align="center" style="background-color:#DCB5FF;"><div><FONT SIZE="7">評等</FONT></div></td> <td><div align="center"><FONT SIZE="7">{{cSign1}}</FONT></div></td>
+</tr>
+
+
+</table>
 </template>
   
 <script>
@@ -18,7 +74,24 @@
     },
   
     props: {
-      cSign6: String
+      a1N: String,
+      a2N: String,
+      a3N: String,
+      a4N: String,
+      a5N: String,
+      a6N: String,
+      a7N: String,
+
+      na1: String,
+      na2: String,
+      na3: String,
+      na4: String,
+      na5: String,
+      na6: String,
+      na7: String,
+      na9: String,
+      na10: String,
+      cSign1: String,
     }, 
 
     data(){
