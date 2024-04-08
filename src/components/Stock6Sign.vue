ddfaeead7ab37ed5d48622cc66d42ea7cabeb16f
pyfbsdk59 <pyfbsdk59@gmail.com>
2024-04-08 09:39:36 +0800

    SFC55

--- src/components/Stock6Sign.vue
+++ src/components/Stock6Sign.vue
@@ -13,67 +13,13 @@
   </div>
   <br>
   
-<div v-if="cSign1">
 
-<table border="2" cellpadding="1" cellspacing="5"  style="border:1px solid black;margin-left:auto;margin-right:auto;">
-<caption><FONT SIZE="6">六大指標</FONT></caption>
-<tr>	
-<td style="background-color:#D3A4FF;" align="center"><div><FONT SIZE="7">台股代號：</FONT></div></td>
-<td align="center"><div><FONT SIZE="7">{{inputText2}}</FONT></div></td>
-</tr>
-
-<tr>	
-<td style="background-color:#D3A4FF;" align="center"><div><FONT SIZE="7">股票名稱：</FONT></div></td>
-<td align="center"><div><FONT SIZE="7">{{cStockName}}</FONT></div></td>
-</tr>
-
-<tr>	
-<td style="background-color:#D3A4FF;" align="center"><div><FONT SIZE="7">最新財報季度：</FONT></div></td>
-<td align="center"><div><FONT SIZE="7">{{cNewestSeason}}</FONT></div></td>
-</tr>
-
-<tr>	
-<td style="background-color:#FFA6FF;" align="center"><div><FONT SIZE="7">營收評等：</FONT></div></td>
-<td align="center"><div><FONT SIZE="7">{{cSign1}}</FONT></div></td>
-</tr>
-
-<tr>	
-<td style="background-color:#FFA6FF;" align="center"><div><FONT SIZE="7">營益率評等：</FONT></div></td>
-<td align="center"><div><FONT SIZE="7">{{cSign2}}</FONT></div></td>
-</tr>
-
-<tr>	
-<td style="background-color:#FFA6FF;" align="center"><div><FONT SIZE="7">稅後淨利評等：</FONT></div></td>
-<td align="center"><div><FONT SIZE="7">{{cSign3}}</FONT></div></td>
-</tr>
-
-<tr>	
-<td style="background-color:#FFA6FF;" align="center"><div><FONT SIZE="7">EPS評等：</FONT></div></td>
-<td align="center"><div><FONT SIZE="7">{{cSign4}}</FONT></div></td>
-</tr>
-
-<tr>	
-<td style="background-color:#FFA6FF;" align="center"><div><FONT SIZE="7">存貨週轉率評等：</FONT></div></td>
-<td align="center"><div><FONT SIZE="7">{{cSign5}}</FONT></div></td>
-</tr>
-
-<tr>	
-<td style="background-color:#FFA6FF;" align="center"><div><FONT SIZE="7">現金流量評等：</FONT></div></td>
-<td align="center"><div><FONT SIZE="7">{{cSign6}}</FONT></div></td>
-</tr>
-
-<tr>	
-<td style="background-color:#FF2D2D;" align="center"><div><FONT SIZE="7">總平均：</FONT></div></td>
-<td align="center"><div><FONT SIZE="7">{{average6stock}}</FONT></div></td>
-</tr>
-</table>
-
- </div>
  <br>
- <br>
-
-<stock-s1gn :cSign6="stock6data.cSign6"></stock-s1gn>
+ 
 <total-score v-bind="stock6data2"></total-score>
+ <br>
+<stock-s1gn :cSign6="stock6data.cSign6"></stock-s1gn>
+
 </template>
 
 <script>
--- src/components/TotalScore.vue
+++ src/components/TotalScore.vue
@@ -1,11 +1,62 @@
 <template>
     
 
-    <div>
-    <FONT SIZE="7">666666</FONT>  
-    <FONT SIZE="7">{{ cSign5 }}</FONT>  
-    <FONT SIZE="7">{{ cSign6 }}</FONT>  
-  </div>
+<div v-if="cSign1">
+
+<table border="2" cellpadding="1" cellspacing="5"  style="border:1px solid black;margin-left:auto;margin-right:auto;">
+<caption><FONT SIZE="6">六大指標</FONT></caption>
+<tr>	
+<td style="background-color:#D3A4FF;" align="center"><div><FONT SIZE="7">台股代號：</FONT></div></td>
+<td align="center"><div><FONT SIZE="7">{{inputText2}}</FONT></div></td>
+</tr>
+
+<tr>	
+<td style="background-color:#D3A4FF;" align="center"><div><FONT SIZE="7">股票名稱：</FONT></div></td>
+<td align="center"><div><FONT SIZE="7">{{cStockName}}</FONT></div></td>
+</tr>
+
+<tr>	
+<td style="background-color:#D3A4FF;" align="center"><div><FONT SIZE="7">最新財報季度：</FONT></div></td>
+<td align="center"><div><FONT SIZE="7">{{cNewestSeason}}</FONT></div></td>
+</tr>
+
+<tr>	
+<td style="background-color:#FFA6FF;" align="center"><div><FONT SIZE="7">營收評等：</FONT></div></td>
+<td align="center"><div><FONT SIZE="7">{{cSign1}}</FONT></div></td>
+</tr>
+
+<tr>	
+<td style="background-color:#FFA6FF;" align="center"><div><FONT SIZE="7">營益率評等：</FONT></div></td>
+<td align="center"><div><FONT SIZE="7">{{cSign2}}</FONT></div></td>
+</tr>
+
+<tr>	
+<td style="background-color:#FFA6FF;" align="center"><div><FONT SIZE="7">稅後淨利評等：</FONT></div></td>
+<td align="center"><div><FONT SIZE="7">{{cSign3}}</FONT></div></td>
+</tr>
+
+<tr>	
+<td style="background-color:#FFA6FF;" align="center"><div><FONT SIZE="7">EPS評等：</FONT></div></td>
+<td align="center"><div><FONT SIZE="7">{{cSign4}}</FONT></div></td>
+</tr>
+
+<tr>	
+<td style="background-color:#FFA6FF;" align="center"><div><FONT SIZE="7">存貨週轉率評等：</FONT></div></td>
+<td align="center"><div><FONT SIZE="7">{{cSign5}}</FONT></div></td>
+</tr>
+
+<tr>	
+<td style="background-color:#FFA6FF;" align="center"><div><FONT SIZE="7">現金流量評等：</FONT></div></td>
+<td align="center"><div><FONT SIZE="7">{{cSign6}}</FONT></div></td>
+</tr>
+
+<tr>	
+<td style="background-color:#FF2D2D;" align="center"><div><FONT SIZE="7">總平均：</FONT></div></td>
+<td align="center"><div><FONT SIZE="7">{{average6stock}}</FONT></div></td>
+</tr>
+</table>
+
+ </div>
     
 </template>
       
